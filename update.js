685fe7419f1345f707cebbcf8d0aed04f873a400
Marco-PC\Marco <epossmurfmarco@gmail.com>
2022-02-15 12:48:13 +0200

    updating upate.js v2

--- update.js
+++ update.js
@@ -14,11 +14,11 @@ export async function main(ns) {
 
   let files = ["scripts/copy-to-server.js", "scripts/createProgram.js"];
 
-  array.forEach((file) => {
+  for (let x = 0; x < files.length; x++) {
     //https://raw.githubusercontent.com/WimpyLigteBottel/bitrunner/master/scripts/copy-to-server.js
-    let newFile = baseUrl + file;
-    await ns.wget(newFile, file, "home");
-  });
+    let newFile = baseUrl + files[x];
+    await ns.wget(newFile, files[x], "home");
+  }
 
   if (args.includes("--lite")) {
     ns.print("it contains it");
