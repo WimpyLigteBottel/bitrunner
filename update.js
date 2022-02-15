5074366b84961abbbd26ae99713491dd09ef2127
Marco-PC\Marco <epossmurfmarco@gmail.com>
2022-02-15 12:40:28 +0200

    Updating update.js

--- update.js
+++ update.js
@@ -1,9 +1,13 @@
 let args = [];
 /** @param {import("./scripts").NS } ns */
 export async function main(ns) {
-    args = arguments["0"].args;
-    
-    ns.wget("")
+  args = arguments["0"].args;
+
+  await ns.wget(
+    "https://raw.githubusercontent.com/WimpyLigteBottel/bitrunner/master/update.js",
+    "update.js",
+    "home"
+  );
 
   if (args.includes("--lite")) {
     ns.print("it contains it");
