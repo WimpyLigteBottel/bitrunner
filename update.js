eafdd9c4694155d640a222f28b900702b8d236e6
Marco-PC\Marco <epossmurfmarco@gmail.com>
2022-02-15 12:46:20 +0200

    updating update.js

--- update.js
+++ update.js
@@ -9,6 +9,17 @@ export async function main(ns) {
     "home"
   );
 
+  let baseUrl =
+    "https://raw.githubusercontent.com/WimpyLigteBottel/bitrunner/master/";
+
+  let files = ["scripts/copy-to-server.js", "scripts/createProgram.js"];
+
+  array.forEach((file) => {
+    //https://raw.githubusercontent.com/WimpyLigteBottel/bitrunner/master/scripts/copy-to-server.js
+    let newFile = baseUrl + file;
+    await ns.wget(newFile, file, "home");
+  });
+
   if (args.includes("--lite")) {
     ns.print("it contains it");
   }
