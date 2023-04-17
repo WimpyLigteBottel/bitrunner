7cac3325b21d80662fd18cc6d2bb6400d9a99381
Marco-PC\Marco <epossmurfmarco@gmail.com>
2023-04-17 17:37:05 +0200

    adding changes

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
   ns.print("Starting script here");
-  await ns.hack("foodnstuff"); //Use Netscript hack function
+  await ns.hack("n00dles"); //Use Netscript hack function
   ns.print(ns.args); //The script arguments must be prefaced with ns as well
 }
