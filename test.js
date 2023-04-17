9267806900057ba25f8a766c50023fc0b68f1528
Marco-PC\Marco <epossmurfmarco@gmail.com>
2023-04-17 17:09:47 +0200

    Starting from scratch

--- test.js
+++
@@ -1,9 +0,0 @@
-let args = [];
-/** @param {import("./scripts").NS } ns */
-export async function main(ns) {
-  args = arguments["0"].args;
-
-  if (args.includes("--lite")) {
-    ns.print("it contains it");
-  }
-}
