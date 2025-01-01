e2b1659c3150cc8fc1fcd7c5795f01e54aaf1538
mnel <epos.smurfmarco@gmail.com>
2025-01-01 16:24:32 +0100

    update

--- scripts/hack.js
+++ scripts/hack.js
@@ -1,12 +1,35 @@
 /** @param {NS} ns */
 export async function main(ns) {
 
-  var hostname = ns.getHostname()
+  ns.print("--------------")
 
-  ns.print("Hostname: "+ hostname)
+  var { hostname, hackTime, hackLevel } = await currentServerStats(ns)
 
-  var analyze = ns.hackAnalyzeSecurity(1,hostname)
+  ns.print(`Current Hostname: ${hostname}`)
+  ns.print(`Current hackTime: ${hackTime}`)
+  ns.print(`Current hackLevel: ${hackLevel}`)
 
-  ns.print("Security: "+ analyze)
+  var servers = await serversConnectedToo(ns, hostname)
 
+}
+
+async function currentServerStats(ns) {
+  var currentServer = {
+    hostname: ns.getHostname().toString(),
+    hackTime: ns.getHackTime().toString(),
+    hackLevel: ns.getHackingLevel().toString()
+  }
+
+  return currentServer
+}
+
+
+async function serversConnectedToo(ns, hostname) {
+  var servers = await ns.scan(hostname)
+
+  for (var index in servers) {
+    ns.print(`${hostname} -> ${servers[index]}`)
+  }
+
+  return servers
 }
