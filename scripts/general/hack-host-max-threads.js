31c20b7ae82c72783802cfde78f94d2c5a2eed83
mnel <epos.smurfmarco@gmail.com>
2025-01-02 00:46:24 +0100

    update

--- scripts/general/hack-host-max-threads.js
+++ scripts/general/hack-host-max-threads.js
@@ -6,7 +6,7 @@ export async function main(ns) {
 
   let hostname =  `${ns.args[0]}`
 
-  let weakenCost = 3
+  let weakenCost = ns.getScriptRam("scripts/general/hack-host.js",ns.getHostname())
   let maxPossibleThreads = Math.round(ns.getServerMaxRam(ns.getHostname()) / weakenCost) - 1
   await ns.exec("scripts/general/hack-host.js",ns.getHostname(), maxPossibleThreads, `${hostname}`)
 
