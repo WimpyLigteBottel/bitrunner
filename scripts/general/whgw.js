5c2b5b54664447e8617d4c0af6ffc5fbf6d1a9fd
mnel <epos.smurfmarco@gmail.com>
2025-01-05 15:40:20 +0100

    update

--- scripts/general/whgw.js
+++ scripts/general/whgw.js
@@ -15,7 +15,8 @@ export async function main(ns) {
     await ns.hack(hostname, { stock: true })
     await ns.sleep(weakenStartTime - hackStartTime + delay)
     await ns.grow(hostname, { stock: true })
-    await ns.sleep(growStartTime - weakenStartTime + delay)
+    await ns.sleep(gro
+    wStartTime - weakenStartTime + delay)
     await ns.weaken(hostname, { stock: true })
   }
 
